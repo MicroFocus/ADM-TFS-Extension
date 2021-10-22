ed602a12357937d3f43ff970192250717abfa56d
Dorin Bogdan <dorin.bogdan@microfocus.com>
2021-10-22 18:47:05 +0300

    DEF-690028 [ADO][ALM Lab Mgmt] Pipeline console logs are cryptic

--- UFTBuildTask/AlmLabManagementTask/localTask.ps1
+++ UFTBuildTask/AlmLabManagementTask/localTask.ps1
@@ -105,6 +105,7 @@ if ((Test-Path $runSummary) -and (Test-Path $uftReport)) {
 	} else {
 		Write-Host "##vso[task.addattachment type=Distributedtask.Core.Summary;name=Reports;]$all"
 	}
+}
 
 # read return code
 if (Test-Path $retcodefile) {
