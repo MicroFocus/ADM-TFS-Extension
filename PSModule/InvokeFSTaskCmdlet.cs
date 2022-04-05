bf80b491a9c7a3f2530131b0e523374b7c2f3117
Dorin Bogdan <dorin.bogdan@microfocus.com>
2022-04-05 22:19:27 +0300

    DEF-775034 [ADO][ParallerRunner] Executable runs regardless of applicable devices

--- PSModule/InvokeFSTaskCmdlet.cs
+++ PSModule/InvokeFSTaskCmdlet.cs
@@ -2,12 +2,25 @@
 using System.Linq;
 using System.Collections.Generic;
 using PSModule.UftMobile.SDK.UI;
+using System;
+using PSModule.UftMobile.SDK.Interface;
+using PSModule.UftMobile.SDK.Auth;
+using PSModule.UftMobile.SDK.Util;
+using PSModule.UftMobile.SDK;
+using System.Threading.Tasks;
+using PSModule.UftMobile.SDK.Entity;
 
 namespace PSModule
 {
     [Cmdlet(VerbsLifecycle.Invoke, "FSTask")]
     public class InvokeFSTaskCmdlet : AbstractLauncherTaskCmdlet
     {
+        private const string DEBUG_PREFERENCE = "DebugPreference";
+        private const string LOGIN_FAILED = "Login failed";
+        private const string DEVICES_ENDPOINT = "rest/devices";
+        private const string REGISTERED = "registered";
+        private const string UNREGISTERED = "unregistered";
+
         [Parameter(Position = 0, Mandatory = true)]
         public string TestsPath { get; set; }
 
@@ -77,6 +90,19 @@ namespace PSModule
             }
         }
 
+        [Parameter(Position = 13)]
+        public MobileConfig MobileConfig
+        {
+            get
+            {
+                return _mobileConfig;
+            }
+            set
+            {
+                _mobileConfig = value;
+            }
+        }
+
         protected override bool CollateResults(string resultFile, string resdir)
         {
             return true; //do nothing here. Collate results should be made by the standard "Copy and Publish Artifacts" TFS task
@@ -84,7 +110,7 @@ namespace PSModule
 
         public override Dictionary<string, string> GetTaskProperties()
         {
-            LauncherParamsBuilder builder = new LauncherParamsBuilder();
+            LauncherParamsBuilder builder = new();
 
             builder.SetRunType(RunType.FileSystem);
             builder.SetPerScenarioTimeOut(Timeout);
@@ -128,6 +154,7 @@ namespace PSModule
                     //TOOO
                 }
             }
+            builder.SetMobileConfig(_mobileConfig);
 
             return builder.GetProperties();
         }
@@ -135,6 +162,81 @@ namespace PSModule
         protected override string GetRetCodeFileName()
         {
             return "TestRunReturnCode.txt";
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (_isParallelRunnerMode)
+            {
+                ValidateDevices().Wait();
+            }
+            base.ProcessRecord();
+        }
+
+        private async Task ValidateDevices()
+        {
+            WriteDebug("Validating the devices....");
+            var deviceIds = ParallelRunnerConfig.Devices.Select(d => d.DeviceId).Where(id => !string.IsNullOrWhiteSpace(id));
+            if (deviceIds.Any())
+            {
+                var allDevices = await GetAllDevices();
+                GetAllDeviceIds(allDevices, out IList<string> onlineDeviceIds, out IList<string> offlineDeviceIds);
+                if (offlineDeviceIds.Any())
+                {
+                    var invalidDeviceIds = deviceIds.Intersect(offlineDeviceIds);
+                    if (invalidDeviceIds.Any())
+                    {
+                        var ids = invalidDeviceIds.Aggregate((a, b) => $"{a}, {b}");
+                        ThrowTerminatingError(new(new($"Disconnected devices are not allowed: {ids}"), nameof(ValidateDevices), ErrorCategory.InvalidData, nameof(ValidateDevices)));
+                    }
+
+                }
+                if (onlineDeviceIds.Any())
+                {
+                    var invalidDeviceIds = deviceIds.Except(onlineDeviceIds);
+                    if (invalidDeviceIds.Any())
+                    {
+                        var ids = invalidDeviceIds.Aggregate((a, b) => $"{a}, {b}");
+                        ThrowTerminatingError(new(new($"Invalid device IDs: {ids}"), nameof(ValidateDevices), ErrorCategory.InvalidData, nameof(ValidateDevices)));
+                    }
+                }
+                else
+                {
+                    ThrowTerminatingError(new(new($"No available devices found."), nameof(ValidateDevices), ErrorCategory.DeviceError, nameof(ValidateDevices)));
+                }
+            }
+        }
+
+        private void GetAllDeviceIds(IList<Device> allDevices, out IList<string> onlineDeviceIds, out IList<string> offlineDeviceIds)
+        {
+            var devices = allDevices.GroupBy(d => d.DeviceStatus).ToList();
+            onlineDeviceIds = devices.FirstOrDefault(g => g.Key == REGISTERED)?.Select(d => d.DeviceId).ToList() ?? new();
+            offlineDeviceIds = devices.FirstOrDefault(g => g.Key == UNREGISTERED)?.Select(d => d.DeviceId).ToList() ?? new();
+        }
+
+        private async Task<IList<Device>> GetAllDevices()
+        {
+            IAuthenticator auth = new BasicAuthenticator();
+            Credentials cred = new (_mobileConfig.Username, _mobileConfig.Password);
+            bool isDebug = (ActionPreference)GetVariableValue(DEBUG_PREFERENCE) != ActionPreference.SilentlyContinue;
+            IClient client = new RestClient(_mobileConfig.ServerUrl, cred, new ConsoleLogger(isDebug));
+            bool ok = await auth.Login(client);
+            if (ok)
+            {
+                var res = await client.HttpGet<Device>(client.ServerUrl.AppendSuffix(DEVICES_ENDPOINT));
+                if (res.IsOK)
+                {
+                    return res.Entities;
+                }
+                else
+                    ThrowTerminatingError(new(new(res.Error), nameof(ValidateDevices), ErrorCategory.DeviceError, nameof(ValidateDevices)));
+
+                await auth.Logout(client);
+            }
+            else
+                ThrowTerminatingError(new(new(LOGIN_FAILED), nameof(ValidateDevices), ErrorCategory.DeviceError, nameof(ValidateDevices)));
+
+            return new List<Device>();
         }
     }
 }
