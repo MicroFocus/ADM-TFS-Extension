63254a9e6e032431af8285d5b737d61e920ee2d8
OPENTEXT\adumitras <adumitras@opentext.com>
2026-07-06 13:46:19 +0300

    fix: enhance worning about GUID from DL

--- PSModule/InvokeFSTaskCmdlet.cs
+++ PSModule/InvokeFSTaskCmdlet.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * MIT License https://github.com/MicroFocus/ADM-TFS-Extension/blob/master/LICENSE
  *
- * Copyright 2016-2024 Open Text
+ * Copyright 2016-2026 Open Text
  *
  * The only warranties for products and services of Open Text and its affiliates and licensors ("Open Text") are as may be set forth in the express warranty statements accompanying such products and services.
  * Nothing herein should be construed as constituting an additional warranty.
@@ -242,13 +242,16 @@ namespace PSModule
             builder.SetDigitalLabSrvConfig(_dlServerConfig);
             builder.SetMobileConfig(_deviceConfig);
             builder.SetCloudBrowserConfig(_cloudBrowserConfig);
-            if (WorkspaceID.IsNullOrWhiteSpace())
+            if (_dlServerConfig != null || _deviceConfig != null || _cloudBrowserConfig != null)
             {
-                WriteWarning($"Workspace ID is empty. A valid GUID is expected.");
-            }
-            else if (!Guid.TryParse(WorkspaceID, out _))
-            {
-                WriteWarning($"Invalid Workspace ID format: '{WorkspaceID}'. A valid GUID is expected.");
+                if (WorkspaceID.IsNullOrWhiteSpace())
+                {
+                    WriteWarning($"Workspace ID is empty. A valid GUID is expected.");
+                }
+                else if (!Guid.TryParse(WorkspaceID, out _))
+                {
+                    WriteWarning($"Invalid Workspace ID format: '{WorkspaceID}'. A valid GUID is expected.");
+                }
             }
             builder.SetWorkspaceID(WorkspaceID);
             if (RunAsDifferentUser)
